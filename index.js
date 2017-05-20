386cab19d457eedb330ec4047ee7bd504f5c86a0
github username <github email address>
2017-05-20 19:31:33 -0400

    Done.

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ function handlebarsSetup() {
 $(document).ready(function (){
   handlebarsSetup()
 });
+
+function searchRepositories() {
+  var searched = $('#searchTerms').val()
+  $.get(`https://api.github.com/search/repositories?q=` + searched, function(response) {
+    const template = Handlebars.compile($('#repo-template').html())
+    $('#results').html(template(response))
+  }).fail(displayError())
+}
+
+function displayError() {
+    $('#errors').html("There has been an error. Please try again.")
+}
+
+function showCommits(el) {
+  const owner = el.dataset.owner
+  const repo = el.dataset.repository
+  $.get('https://api.github.com/repos/' + owner + '/' + repo + '/commits', function(response){
+    const template = Handlebars.compile($('#commits-template').html())
+    $('#details').html(template(response))
+  }).fail(displayError())
+}
